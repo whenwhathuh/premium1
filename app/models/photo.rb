d773c29c531fbfaa9d96cdc0c7de0d5ecaec07a6
Johnny Rutkowski <rockicestrings@gmail.com>
2013-07-07 19:34:35 -0400

    Add Paperclip to Photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,11 @@
 class Photo < ActiveRecord::Base
-  attr_accessible :description, :album_id
-
-  belongs_to :album
+  attr_accessible :description, :album_id, :image
+ 
   validates :album_id, presence: true
   validates :description, presence: :true
+  validates_attachment :image, presence: true,
+                        content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] }
+
+  belongs_to :album
+  has_attached_file :image, styles: { medium: "320x240>"}
 end
